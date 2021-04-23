1497958d6947f5c64f8dd44db2c2364aa2954649
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-22 17:02:54 -0700

    Implemented "Not Interested" button click functionality
    App.js
    - Created update function (removeTour) that takes an ID that will create a completely new array by copying over all Tour components, except for the one with and ID that matches the ID passed in to the function. Then set this new state by calling the setTours function with the new array
    - Pass the removeTour function down to the Tours component via props
    
    Tours.js
    - Accept the passed in removeTour function via prop
    - Forward it down to the Tour component via prop.
    
    Tour.js
    - Accept the pass in removeTour function via prop
    - Set the onClick for the "Not Interested" button to call the removeTour function and pass in the ID value of the current Tour as required by removeTour.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -32,7 +37,7 @@ function App() {
   }
   
   return <main>
-    <Tours tours={tours}/>
+    <Tours tours={tours} removeTour={removeTour}/>
   </main>
 
 }
