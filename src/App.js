523b60954dfeac953a1ff195dc200f1c5d05093e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-22 17:07:56 -0700

    Added Heading for no tours left
    - If all the tours are removed, then return a "No Tours Left" heading, and a refresh button.
    - onClick of refresh button just fetches all tours again from the API

--- src/App.js
+++ src/App.js
@@ -35,6 +35,15 @@ function App() {
       <Loading/>
     </main>
   }
+
+  if (tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={fetchTours}>refresh</button>
+      </div>
+    </main>
+  }
   
   return <main>
     <Tours tours={tours} removeTour={removeTour}/>
